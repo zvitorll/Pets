32d4c8fad7b37bd9a4b9d8340e691dc8990f53d2
Gabriel Vitor <125525068+zvitorll@users.noreply.github.com>
2024-12-29 10:12:45 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+document.getElementById("Loginbutton").onclick = function() {Loginclick()};
+
+const write = ([data, filename]) => {
+}
+
+let CPFjs = document.getElementById("CPF").value;
+function Loginclick() {
+    console.log("Registrado")
+}
